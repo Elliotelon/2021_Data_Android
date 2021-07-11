9c3ae37fb337a41d6adeb790408a6dcc4dd9403e
kimminkyu <kmk50009@naver.com>
2021-07-11 14:01:48 +0900

    Data Step26 NinePatchImage

---
+++ step26ninepatchimage/src/main/java/study/data/step26ninepatchimage/MainActivity.kt
@@ -0,0 +1,11 @@
+package study.data.step26ninepatchimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
