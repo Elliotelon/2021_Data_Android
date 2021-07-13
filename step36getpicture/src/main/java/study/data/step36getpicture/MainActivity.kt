a4591218d40abde65f7aaa7dbaf0f2000e928e57
kimminkyu <kmk50009@naver.com>
2021-07-14 00:33:19 +0900

    Data Step36 GetPicture(Update1)

--- step36getpicture/src/main/java/study/data/step36getpicture/MainActivity.kt
+++ step36getpicture/src/main/java/study/data/step36getpicture/MainActivity.kt
@@ -3,7 +3,10 @@ package study.data.step36getpicture
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -57,6 +60,7 @@ class MainActivity : AppCompatActivity() {
             1 -> {
                 if(resultCode == RESULT_OK){
                     val bitmap = data?.getParcelableExtra<Bitmap>("data")
+
                     imageView.setImageBitmap(bitmap)
                 }
             }
@@ -64,7 +68,13 @@ class MainActivity : AppCompatActivity() {
             2 -> {
                 if(resultCode == RESULT_OK){
                     val bitmap = BitmapFactory.decodeFile(contentUri.path)
-                    imageView.setImageBitmap(bitmap)
+
+                    val degree = getDegree(contentUri, contentUri.path!!)
+
+                    val bitmap2 = resizeBitmap(1024, bitmap)
+                    val bitmap3 = rotateBitmap(bitmap2, degree)
+
+                    imageView.setImageBitmap(bitmap3)
 
                     //사진 파일 삭제한다.
                     val file = File(contentUri.path)
@@ -72,5 +82,47 @@ class MainActivity : AppCompatActivity() {
                 }
            }
         }
+    }
+    //사진의 사이즈를 조정하는 메소드
+    fun resizeBitmap(targetWidth:Int, source:Bitmap):Bitmap{
+        //이미지의 비율을 계산한다.
+        val ratio = targetWidth.toDouble()/source.width.toDouble()
+        //보정될 세로 길이를 구한다.
+        val targetHeight = (source.height*ratio).toInt()
+        //크기를 조정한 bitmap객체를 생성한다.
+        val result = Bitmap.createScaledBitmap(source, targetWidth, targetHeight, false)
+        return result
+    }
+
+    //이미지의 회전 각도값을 구한다.
+    fun getDegree(uri:Uri, source:String):Float{
+
+        var exif:ExifInterface? = null
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            val photoUri = MediaStore.setRequireOriginal(uri)
+            val stream = contentResolver.openInputStream(photoUri)
+            exif = ExifInterface(stream!!)
+        } else {
+            exif = ExifInterface(source)
+        }
+
+        var degree = 0
+        val ori = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1)
+        when(ori){
+            ExifInterface.ORIENTATION_ROTATE_90 -> degree = 90
+            ExifInterface.ORIENTATION_ROTATE_180 -> degree = 180
+            ExifInterface.ORIENTATION_ROTATE_270 -> degree = 270
+        }
+        return degree.toFloat()
+    }
+
+    fun rotateBitmap(bitmap:Bitmap, degree:Float):Bitmap{
+        //각도값을 관리하는 객체
+        val matrix = Matrix()
+        matrix.postRotate(degree)
+        //회전된 이미지를 받아온다.
+        val bitmap2 = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, false)
+        return bitmap2
     }
 }
